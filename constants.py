79c733f3f34d09ddf695cdaffec19a727d5f3831
Wes Young <wes@barely3am.com>
2018-06-28 12:21:18 -0400

    ..

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
 
 BATCH_SIZE = os.getenv('BATCH_SIZE', 256)
-MAX_STRING_LEN = 255
+MAX_STRING_LEN = os.getenv('MAX_STRING_LEN', 255)
 MODEL = os.getenv('MODEL', 'model.h5')
 WEIGHTS = os.getenv('WEIGHTS', 'weights.h5')
